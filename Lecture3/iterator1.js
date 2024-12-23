4d245aab12483a85c10d27150a02974182769ec7
pennapha1233 <147477065+pennapha1233@users.noreply.github.com>
2024-12-23 21:55:11 +0700

    Add files via upload

---
+++ Lecture3/iterator1.js
@@ -0,0 +1,4 @@
+for (let char of "test") {
+    // triggers 4 times: once for each characher
+    console.log( char ); // t, then e, then s, then t,
+}
---
+++ Lecture3/objDemo.js
@@ -0,0 +1,25 @@
+/* START */
+
+// How can you represent real-world objects in code?
+// They have associated attributes = object PROPERTIES
+// They have associated actions = object METHOD
+// They have associated context = "this"
+
+// First:
+// Let's define a simple object (with no properties or method)
+const blank = {};
+console.log("Blank type:,typeof blank");
+console.log("Blank value",blank);
+
+/*
+// Next:
+// Let's define an object with PROPERTIES (attributes)
+// objects are just a coolection of name-value pairs
+// separated by commas
+cons book = {
+    title : "1984",
+    author: "George Orwell",
+    isAvailable: flase
+}: */
+
+
